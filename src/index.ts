1a8309e7692f2011179bce4d36397d1c1197fade
Dhananjay Chavan <219833325+cdhananjay@users.noreply.github.com>
2026-06-01 12:48:15 +0530

    feat(better-auth): setup better auth

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import type {Response, Request} from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import morgan from "morgan";
+
 // Create a new express application instance
 const app = express();
 
 // Set the network port
 const port = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+// Mount express json middleware after Better Auth handler
+// or only apply it to routes that don't interact with Better Auth
+app.use(express.json());
 
 // Define the root path with a greeting message
 app.get("/", (req: Request, res: Response) => {
